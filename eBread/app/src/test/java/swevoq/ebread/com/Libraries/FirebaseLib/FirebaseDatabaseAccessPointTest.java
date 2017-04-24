899c6131acc96e2cd621282d82d3a540c8cb29d0
Unknown <marco.giorgio1993@gmail.com>
2017-04-24 18:25:00 +0200

    Test di unità

---
+++ eBread/app/src/test/java/swevoq/ebread/com/Libraries/FirebaseLib/FirebaseDatabaseAccessPointTest.java
@@ -0,0 +1,78 @@
+package swevoq.ebread.com.Libraries.FirebaseLib;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class FirebaseDatabaseAccessPointTest {
+    @Test
+    public void saveUser() throws Exception {
+
+    }
+
+    @Test
+    public void getChats() throws Exception {
+
+    }
+
+    @Test
+    public void getUserData() throws Exception {
+
+    }
+
+    @Test
+    public void getLastMessage() throws Exception {
+
+    }
+
+    @Test
+    public void newMessage() throws Exception {
+
+    }
+
+    @Test
+    public void deleteSelectedMessages() throws Exception {
+
+    }
+
+    @Test
+    public void getMessages() throws Exception {
+
+    }
+
+    @Test
+    public void addUserToAddressBook() throws Exception {
+
+    }
+
+    @Test
+    public void getFriends() throws Exception {
+
+    }
+
+    @Test
+    public void createChat() throws Exception {
+
+    }
+
+    @Test
+    public void deleteFriends() throws Exception {
+
+    }
+
+    @Test
+    public void deleteChat() throws Exception {
+
+    }
+
+    @Test
+    public void getUsers() throws Exception {
+
+    }
+
+    @Test
+    public void setContactVoice() throws Exception {
+
+    }
+}
