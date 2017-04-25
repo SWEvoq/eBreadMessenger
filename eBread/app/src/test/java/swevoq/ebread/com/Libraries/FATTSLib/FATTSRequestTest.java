dffe25623c7d4a253178268b0f902754d7076f14
Unknown <marco.giorgio1993@gmail.com>
2017-04-25 18:15:12 +0200

    Test Unità

---
+++ eBread/app/src/test/java/swevoq/ebread/com/Libraries/FATTSLib/FATTSRequestTest.java
@@ -0,0 +1,23 @@
+package swevoq.ebread.com.Libraries.FATTSLib;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FATTSRequestTest {
+    @Test
+    public void getParams() throws Exception {
+
+    }
+
+    @Test
+    public void deliverResponse() throws Exception {
+
+    }
+
+    @Test
+    public void parseNetworkResponse() throws Exception {
+
+    }
+
+}
